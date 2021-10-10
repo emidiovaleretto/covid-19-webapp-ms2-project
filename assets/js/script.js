f99c3684f8b7c9a1df06d7da4ecee42a6fe5ccea
Emidio Valereto <emidio.valereto@gmail.com>
2021-10-10 19:25:42 +0100

    Fix background color in boxes when click on the icon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
-function changeBackground(event) {
-    let body = document.body; 
-    body.style.backgroundColor = 'white';
+function changeBackgroundColor(event) {
+    let body = document.body;
+    let box = document.getElementsByClassName("box");
+    let icon = document.getElementsByClassName('btn-light')[0];
+    icon.classList.replace('btn-light', 'btn-warning');
+
+    if (body.id === 'darkgray') {
+        body.style.backgroundColor = '#FFFFFF';
+
+        for (let i = 0; i < box.length; i++) {
+            box[i].style.backgroundColor = '#e9e9e9';
+            box[i].style.color = '#000000';
+            box[i].style.boxShadow = '8px 8px 10px #3B3C39';
+        }
+    }
+    else {
+        body.style.backgroundColor = '#181818';
+    }
 }
 
 let clickableIcon = document.getElementById('clickable-icon');
-clickableIcon.addEventListener('click', changeBackground);
+clickableIcon.addEventListener('click', changeBackgroundColor);
