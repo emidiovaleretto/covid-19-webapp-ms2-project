ffeaf063cc2357735121b6c6ebd01c0ced8da4cb
Emidio Valereto <emidio.valereto@gmail.com>
2021-10-11 19:59:11 +0100

    Implement API to access data.

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,46 @@ function changeBackgroundColor(event) {
 
 let icon = document.getElementById('clickable-icon');
 icon.addEventListener('click', changeBackgroundColor);
+
+//create empty array
+let global = [];
+let country = [];
+
+
+window.addEventListener("load", function () {
+    api();
+
+});
+
+
+const url = 'https://api.covid19api.com/summary';
+async function api() {
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const globalData = data.Global;
+    const countries = data.Countries;
+    country = countries;
+    global = globalData;
+    addTextContent();
+}
+
+function addTextContent() {
+
+    lastUpdate.innerHTML = global.Date;
+    newConfirmed.innerHTML = global.NewConfirmed;
+    totalConfirmed.innerHTML = global.TotalConfirmed;
+    dailyDeaths.innerHTML = global.NewDeaths;
+    totalDeaths.innerHTML = global.TotalDeaths;
+    newRecovered.innerHTML = global.NewRecovered;
+    totalRecovered.innerHTML = global.TotalRecovered;
+   
+}
+
+let lastUpdate = document.getElementById('last-updated');
+let newConfirmed = document.getElementById("new-confirmed");
+let totalConfirmed = document.getElementById("total-confirmed");
+let dailyDeaths = document.getElementById("daily-deaths");
+let totalDeaths = document.getElementById("total-deaths");
+let newRecovered = document.getElementById('daily-recovered');
+let totalRecovered = document.getElementById('total-recovered');
